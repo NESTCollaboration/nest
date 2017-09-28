acbc697bd0242792f3c54f971d5f418d50768944
”szydagis” <matthew.szydagis@gmail.com>
2017-09-28 16:43:27 -0400

    added AmBe, plus exposure timing for 8B

--- testNEST.cpp
+++ testNEST.cpp
@@ -30,6 +30,7 @@ int main ( int argc, char** argv ) {
   if (argc < 7) {
     cout << "This program takes 6 inputs." << endl;
     cout << "numEvts type_interaction E_min[keV] E_max[keV] density[g/cm^3] field_drift[V/cm]" << endl;
+    cout << "for 8B or WIMPs, numEvts is kg-days of exposure" << endl;
     return 0;
   }
   
@@ -39,7 +40,7 @@ int main ( int argc, char** argv ) {
   INTERACTION_TYPE type_num;
   if (type == "NR") type_num = NR;
   else if (type == "WIMP")type_num = WIMP;
-  else if (type == "B8") type_num = B8;
+  else if (type == "B8") { type_num = B8; numEvts = (unsigned long int)(floor(atof(argv[1])*1.1/365.+0.5)); }
   else if (type == "DD") type_num = DD;
   else if (type == "AmBe")type_num = AmBe;
   else if (type == "Cf") type_num = Cf;
@@ -49,25 +50,25 @@ int main ( int argc, char** argv ) {
   else if (type == "CH3T")type_num = CH3T;
   else type_num = Kr83m;
   
-  double eMin = atof(argv[3]);
-  double eMax = atof(argv[4]);
+  double eMin = atof(argv[3]); xMin = eMin;
+  double eMax = atof(argv[4]); xMax = eMax;
   double rho = atof(argv[5]);
   double field = atof(argv[6]);
   
   fprintf(stdout, "E [keV]\tNph\tNe-\n");
   NEST::NESTcalc n;
   for (unsigned long int j = 0; j < numEvts; j++) {
+    xyTry[2] = 1.;
     switch (type_num) {
     case CH3T:
       m_e = 510.9989461; //e- rest mass-energy [keV]
       aa = 0.0072973525664; //fine structure constant
       ZZ = 2.;
-      xMax = eMax; if(xMax>18.5898)xMax=18.5898; //tritium beta decay endpoint [keV]
-      xMin = eMin; if(xMin<0.)xMin=0.;
+      if(xMax>18.5898)xMax=18.5898; //tritium beta decay endpoint [keV]
+      if(xMin<0.)xMin=0.;
       yMax = 1.1e7; //top of the beta decay E histogram
       xyTry[0] = xMin+(xMax-xMin)*n.rand_uniform();
       xyTry[1] = yMax * n.rand_uniform();
-      xyTry[2] = 1.;
       while ( xyTry[2] > 0. ) {
 	B = sqrt(xyTry[0]*xyTry[0] + 2.*xyTry[0]*m_e) / (xyTry[0] + m_e);
 	x = (2.*M_PI*ZZ*aa)*(xyTry[0] + m_e)/sqrt(xyTry[0]*xyTry[0] + 2.*xyTry[0]*m_e);
@@ -79,17 +80,26 @@ int main ( int argc, char** argv ) {
       }
       break;
     case B8: //normalize this to ~3500 / 10-ton / year, for E-threshold of 0.5 keVnr, OR 180 evts/t/yr/keV at 1 keV
-      xMax = eMax; if(xMax>3.)xMax=3.;
-      xMin = eMin; if(xMin<0.)xMin=0.;
+      if(xMax>3.)xMax=3.;
+      if(xMin<0.)xMin=0.;
       yMax = 28666.6;
       xyTry[0] = xMin+(xMax-xMin)*n.rand_uniform();
       xyTry[1] = yMax * n.rand_uniform();
-      xyTry[2] = 1.;
       B =   5.8335;
       x = 104.35404758803;
       while ( xyTry[2] > 0. ) {
 	FuncValue = (3484889.3845409*(pow(xyTry[0],B)+2.4276915393899*pow(xyTry[0],B-1.)+x)/pow(pow(xyTry[0],B)+x,2.)-4728.26850918)*pow(.090455252325381,xyTry[0]);
 	xyTry = n.VonNeumann(xMin,xMax,0.,yMax,xyTry[0],xyTry[1],FuncValue);
+      }
+      break;
+    case AmBe: //for ZEPLIN-III FSR from HA (Pal '98)
+      if(xMax>200.)xMax=200.;
+      if(xMin<0.00)xMin=0.00;
+      xyTry[0] = xMin+(xMax-xMin)*n.rand_uniform();
+      xyTry[1] = n.rand_uniform();
+      while ( xyTry[2] > 0. ) {
+	FuncValue = exp(-sqrt(xyTry[0]))*(1.+pow(pow(xyTry[0]/31.566,5.),0.45132));
+	xyTry = n.VonNeumann(xMin,xMax,0.,1.,xyTry[0],xyTry[1],FuncValue);
       }
       break;
     default:

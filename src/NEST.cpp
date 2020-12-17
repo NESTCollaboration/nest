94d503b7bc685d3b67d14b7dcfa24576ca546453
Gratt55 <gman.block@gmail.com>
2020-12-17 13:24:01 -0500

    Update NEST.cpp

--- src/NEST.cpp
+++ src/NEST.cpp
@@ -1432,6 +1432,7 @@ vector<double> NESTcalc::CalculateG2(bool verbosity) {
   double StdDev = 0., Nphe, pulseArea, pulseAreaC, NphdC, phi, posDep, r,x,y; int Nph, nHits;
 	
   if (verbosity) {
+	  
     for ( int i = 0; i < 10000; ++i ) { // calculate properly the width (1-sigma std dev) in the SE size
       Nph = int(floor(RandomGen::rndm()->rand_gauss(elYield,sqrt(fdetector->get_s2Fano()*elYield))+0.5));
       phi = 2.*M_PI*RandomGen::rndm()->rand_uniform();

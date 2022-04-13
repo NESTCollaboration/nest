806dc318d0683ea58b8a1612a6fc7a80e333f302
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-04-13 08:54:23 -0700

    some changes

---
+++ include/NEST/LArNEST.hh
@@ -0,0 +1,22 @@
+/**
+ * @file LArNEST.hh
+ * @author NEST Collaboration
+ * @author Nicholas Carrara [nmcarrara@ucdavis.edu]
+ * @author Justin Mueller [Justin.Mueller@colostate.edu]
+ * @author Michael Mooney [mrmooney@colostate.edu]
+ * @brief 
+ * @version 
+ * @date 2022-04-13
+ */
+#pragma once
+
+#include "NEST.hh"
+
+namespace larnest
+{
+    class LArNEST : public NEST::NESTCalc
+    {
+    public:
+    private:
+    };
+}
---
+++ src/LArNEST.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file LArNEST.cpp
+ * @author NEST Collaboration
+ * @author Nicholas Carrara [nmcarrara@ucdavis.edu]
+ * @author Justin Mueller [Justin.Mueller@colostate.edu]
+ * @author Michael Mooney [mrmooney@colostate.edu]
+ * @brief 
+ * @version
+ * @date 2022-04-13 
+ */
+#include "LArNEST.hh"
+
+namespace larnest
+{
+    
+}
